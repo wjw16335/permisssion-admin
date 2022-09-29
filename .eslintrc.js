f16fe421381baf47c989e50d7971c83a54138a04
辻弌 <1633598066@qq.com>
2022-09-29 19:23:40 +0800

    feat(custom): 配置了prettier以及commintzen

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
+  // 表示当前目录即为根目录，ESLint 规则将被限制到该目录下
   root: true,
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-before-function-paren': 'off'
   }
 }
